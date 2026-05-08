9777ca61da01f2aed497bd9a6a16bbf829a83580
Martin Gratereaux <gratereaux28@gmail.com>
2026-05-08 19:46:45 -0400

    update server port configuration to use environment variable

--- modern.config.ts
+++ modern.config.ts
@@ -1,9 +1,10 @@
 import { appTools, defineConfig } from '@modern-js/app-tools';
 import { bffPlugin } from '@modern-js/plugin-bff';
+import { env } from 'process';
 
 export default defineConfig({
   server: {
-    port: 8080,
+    port: env.PORT ? parseInt(env.PORT) : 3300,
   },
   plugins: [appTools(), bffPlugin()],
 });
